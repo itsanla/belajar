ce0f660f73419ae08e5b206b075bbf17eec6fc0d
itsanla <anlaharpanda@gmail.com>
2026-04-13 18:32:05 +0700

    feat: first setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div >
-      hello world
+    <div>
+      <div className="bg-background">
+        <p>Geist Sans - font-sans</p>
+      </div>
+      <div className="bg-color">
+        <p>Geist Mono - font-mono</p>
+      </div>     
+      <div className="bg-foreground">
+        <p>Geist Mono - font-mono</p>
+      </div>     
     </div>
   );
 }
