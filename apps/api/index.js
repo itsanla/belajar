da00a59d530b9e39b41215c7ce63c176ca40befd
itsanla <anlaharpanda@gmail.com>
2026-04-15 08:34:32 +0700

    feat: update .gitignore, refactor server port configuration, and add npm scripts

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.listen(3001, () => {
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
     console.log('Server Berjalan di Port 3001');
 })
